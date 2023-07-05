d3a79a90d781a5872ba3918fd9c1c9933669e76e
Honk <Honk@Desktop1>
2023-07-05 14:07:34 -0600

    Homework assignment completed. Program working as expected.

--- M3 Assignment/src/M3Assignment.java
+++ M3 Assignment/src/M3Assignment.java
@@ -1,26 +1,56 @@
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+/**
+ * M3Assignment class that takes in data from Module3-Data.txt such as product name and cost. 
+ *  Then outputs product name, cost and price to the console and into a new file called Inventory.txt
+ * 
+ * CSC2040C00
+ * @author Mark Corder S02286163
+ * @date 05-Jul-2023
+ */
 public class M3Assignment {
     
     private String product;
     private double cost;
     private double price;
     
-    public void run() {
-	setProduct("Television");
-	setCost(439.65);
-	printConsole();
+    public void run() throws IOException {
+	
+	FileReader fr = new FileReader(".\\Module3-Data.txt");
+	BufferedReader br = new BufferedReader(fr);
+	
+	FileWriter fw = new FileWriter(".\\Inventory.txt");
+	PrintWriter pw = new PrintWriter(fw);
+	
+	String currentLine;
+	
+	while((currentLine = br.readLine()) != null) {
+	    setProduct(currentLine);
+	    setCost(br.readLine());
+	    printConsole();
+	    pw.println("Product Name: " + this.product);
+	    pw.println("Poduct Cost: $" + this.cost);
+	    pw.println("Product Price: $" + this.price);
+	    pw.println();
+	    pw.flush();
+	}
+	
     }
     
     private void setProduct(String productName) {
 	this.product = productName;
     }
     
-    private void setCost(double productCost) {
-	this.cost = productCost;
-	setUpdatedCost(this.cost);
+    private void setCost(String productCost) {
+	this.cost = Double.parseDouble(productCost);
+	setPrice(this.cost);
     }
     
-    private void setUpdatedCost(double productCost) {
+    private void setPrice(double productCost) {
 	this.price = Math.round(productCost * 130) / 100.0;
     }
     
--- M3 Assignment/src/MainM3Assignment.java
+++ M3 Assignment/src/MainM3Assignment.java
@@ -1,7 +1,16 @@
+import java.io.IOException;
 
+/**
+ * This is the main entry point to the M3Assignment class that takes in data from Module3-Data.txt such as product name
+ *  and cost. Then outputs product name, cost and price to the console and into a new file called Inventory.txt
+ * 
+ * CSC2040C00
+ * @author Mark Corder S02286163
+ * @date 05-Jul-2023
+ */
 public class MainM3Assignment {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 	
 	M3Assignment myM3Assignment = new M3Assignment();
 	myM3Assignment.run();
